a773f35adeea41c281ee13c55503fe0dc1bd6cb4
felix <felixahofstaetter@gmail.com>
2023-01-18 17:18:34 +0100

    Set up nox

---
+++ noxfile.py
@@ -0,0 +1,48 @@
+import tempfile
+
+import nox
+
+locations = "tests", "noxfile.py"
+nox.options.sessions = "lint", "mypy", "tests"
+
+
+@nox.session(python="3.10")
+def black(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+# flake8-black causes a warning if black would make changes
+@nox.session(python=["3.10"])
+def lint(session):
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.10"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
