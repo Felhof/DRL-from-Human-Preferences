0db9a1ae99d6a5e9aecb73fcfd4d5789f974d710
felix <felixahofstaetter@gmail.com>
2023-01-24 20:34:15 +0100

    formatting

--- src/preferences.py
+++ src/preferences.py
@@ -26,15 +26,19 @@ class SegmentDB:
         self.segments.extend(new_segments)
 
     def query_segment_pairs(self, n):
-        assert len(self.segments) >= 2 * n, "Not enough segments to get that many pairs!"
+        assert (
+            len(self.segments) >= 2 * n
+        ), "Not enough segments to get that many pairs!"
 
         all_indices = list(range(len(self.segments)))
         selected_indices = np.random.choice(all_indices, 2 * n, replace=False)
         np.random.shuffle(selected_indices)
         index_pairs = [
-            (selected_indices[i], selected_indices[i + 1]) for i in range(0, len(selected_indices), 2)
+            (selected_indices[i], selected_indices[i + 1])
+            for i in range(0, len(selected_indices), 2)
         ]
         segment_pairs = [
-            (self.segments[index_pair[0]], self.segments[index_pair[1]]) for index_pair in index_pairs
+            (self.segments[index_pair[0]], self.segments[index_pair[1]])
+            for index_pair in index_pairs
         ]
         return segment_pairs
