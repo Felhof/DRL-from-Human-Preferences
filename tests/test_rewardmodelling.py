e2386e6891960994442af2efce7949fe69003f0d
felix <felixahofstaetter@gmail.com>
2023-01-27 12:18:21 +0100

    (reward modelling process wip) add fixtures to make testing easier

--- tests/test_rewardmodelling.py
+++ tests/test_rewardmodelling.py
@@ -1,7 +1,68 @@
 import numpy as np
+import pytest
 import torch
 
 from src.rewardmodelling import PreferenceBuffer, RewardModel, RewardModellingProcess
+
+
+@pytest.fixture
+def reward_modelling_process(mocker):
+    def _create_reward_modelling_process(
+            preference_queue=None,
+            reward_model=None,
+            reward_model_queue=None,
+            stop_queue=None,
+            training_buffer=None,
+            evaluation_buffer=None
+    ):
+        if preference_queue is None:
+            preference_queue = mocker.Mock()
+            reward_model_queue.put = mocker.Mock()
+
+        if reward_model_queue is None:
+            reward_model_queue = mocker.Mock()
+
+        if stop_queue is None:
+            stop_queue = mocker.Mock()
+
+        mocker.patch("src.rewardmodelling.RewardModel",
+                     return_value=reward_model if reward_model is not None else mocker.Mock())
+
+        mocker.patch(
+            "src.rewardmodelling.PreferenceBuffer",
+            side_effect=[
+                training_buffer if training_buffer is not None else mocker.Mock(),
+                evaluation_buffer if evaluation_buffer is not None else mocker.Mock(),
+            ]
+        )
+
+        reward_modeller = RewardModellingProcess(preference_queue, reward_model_queue, stop_queue)
+
+        return reward_modeller
+
+    return _create_reward_modelling_process
+
+
+@pytest.fixture
+def runnable_reward_modelling_process(mocker, reward_modelling_process):
+    def _create(
+            use_for_training=None,
+            **kwargs
+    ):
+        if use_for_training is None:
+            use_for_training = [1]
+        mocker.patch("src.rewardmodelling.np.random.binomial", side_effect=use_for_training)
+
+        if "stop_queue" not in kwargs:
+            stop_queue = mocker.Mock()
+            stop_queue.empty = mocker.Mock(return_value=False)
+            stop_queue.get = mocker.Mock(side_effect=[False, True])
+            kwargs["stop_queue"] = stop_queue
+
+        reward_modeller = reward_modelling_process(**kwargs)
+        return reward_modeller
+
+    return _create
 
 
 def test_preference_buffer_can_add_new_items_up_to_buffer_size_and_loops_afterwards(
@@ -73,29 +134,26 @@ def test_reward_model_maps_observation_to_scalar():
     assert predictions.shape
 
 
-def test_reward_modelling_process_puts_initial_reward_model_in_queue(mocker):
+def test_reward_modelling_process_puts_initial_reward_model_in_queue(mocker, reward_modelling_process):
     # Given
-    preference_queue = mocker.Mock()
     reward_model = mocker.Mock()
-    mocker.patch("src.rewardmodelling.RewardModel", return_value=reward_model)
     reward_model_queue = mocker.Mock()
     reward_model_queue.put = mocker.Mock()
-    stop_queue = mocker.Mock()
 
     # When
-    reward_modelling_process = RewardModellingProcess(
-        preference_queue, reward_model_queue, stop_queue
+    reward_modeller = reward_modelling_process(
+        reward_model=reward_model, reward_model_queue=reward_model_queue
     )
 
     # Then
-    assert reward_modelling_process.reward_model == reward_model
-    assert reward_modelling_process.reward_model_queue == reward_model_queue
-    reward_modelling_process.reward_model_queue.put.assert_called_once_with(
+    assert reward_modeller.reward_model == reward_model
+    assert reward_modeller.reward_model_queue == reward_model_queue
+    reward_modeller.reward_model_queue.put.assert_called_once_with(
         reward_model
     )
 
 
-def test_reward_modelling_process_run_gets_all_preferences_from_queue(mocker):
+def test_reward_modelling_process_run_gets_all_preferences_from_queue(mocker, runnable_reward_modelling_process):
     # Given
     preference_queue = mocker.Mock()
     preference_queue.empty = mocker.Mock(side_effect=[False, False, False, False, False, True])
@@ -106,23 +164,15 @@ def test_reward_modelling_process_run_gets_all_preferences_from_queue(mocker):
     preference5 = mocker.Mock()
     preference_queue.get = mocker.Mock(side_effect=[preference1, preference2, preference3, preference4, preference5])
 
-    mocker.patch("src.rewardmodelling.RewardModel", return_value=mocker.Mock())
-    mocker.patch("src.rewardmodelling.np.random.binomial", side_effect=[1, 1, 1, 0, 1])
-
     training_buffer = mocker.Mock()
     training_buffer.add = mocker.Mock()
     evaluation_buffer = mocker.Mock()
     evaluation_buffer.add = mocker.Mock()
-    mocker.patch("src.rewardmodelling.PreferenceBuffer", side_effect=[training_buffer, evaluation_buffer])
-
-    reward_model_queue = mocker.Mock()
-    stop_queue = mocker.Mock()
-    stop_queue.empty = mocker.Mock(return_value=False)
-    stop_queue.get = mocker.Mock(side_effect=[False, True])
 
     # When
-    reward_modelling_process = RewardModellingProcess(
-        preference_queue, reward_model_queue, stop_queue
+    reward_modelling_process = runnable_reward_modelling_process(
+        preference_queue=preference_queue, training_buffer=training_buffer,
+        evaluation_buffer=evaluation_buffer, use_for_training=[1, 1, 1, 0, 1]
     )
     reward_modelling_process.run()
 
